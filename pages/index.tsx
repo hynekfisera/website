4b21b5d0b9cb9a50d9be802e7de442a1d4e55204
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2022-03-29 09:51:03 +0200

    Add next-i18next

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useTranslation } from "next-i18next";
+
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export async function getStaticProps({ locale }: { locale: string }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
+}
 
 const Home: NextPage = () => {
+  const { t } = useTranslation();
+
   return (
     <>
       <Head>
@@ -10,7 +23,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Hello world!</h1>
+        <h1>{t("hello_world")}</h1>
       </main>
     </>
   );
