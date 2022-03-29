900cfd69657dd7e45d6b1e1898944f72c3474ce1
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2022-03-29 10:15:52 +0200

    Add next-seo

---
+++ next-seo.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  openGraph: {
+    type: "website",
+    url: "https://www.hynekfisera.com/",
+    site_name: "Hynek Fišera",
+  },
+  twitter: {
+    handle: "@hynekfisera",
+    site: "@hynekfisera",
+    //cardType: "summary_large_image",
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
+import { DefaultSeo } from "next-seo";
+import SEO from "../next-seo.config";
 
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
+    </>
+  );
+};
 
 export default appWithTranslation(MyApp);
