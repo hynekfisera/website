7b3270cca2c2e6aeffcd7565479a539d84b2dd5a
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2022-04-02 14:10:02 +0200

    Add projects section

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -43,7 +43,7 @@ export default function Header() {
         <div className={`${visible ? "translate-x-0" : "translate-x-full"} flex flex-col fixed transition-transform duration-300 pt-20 sm:pt-0 pl-8 sm:pl-0 top-0 h-full right-0 w-48 sm:w-auto bg-indigo-500 sm:static sm:bg-transparent gap-4 sm:gap-8 sm:translate-x-0 sm:flex-row`}>
           {links.map((link) => (
             <Link href={link.href} key={link.href}>
-              <a onClick={() => setVisible(false)} className="font-medium sm:font-semibold underline underline-offset-2 sm:no-underline sm:tracking-wide text-gray-50 sm:text-gray-500 sm:transition sm:duration-200 sm:hover:text-gray-700">
+              <a onClick={() => setVisible(false)} className="xl:text-[17px] font-medium sm:font-semibold underline underline-offset-2 sm:no-underline sm:tracking-wide text-gray-50 sm:text-gray-500 sm:transition sm:duration-200 sm:hover:text-gray-700">
                 {link.text}
               </a>
             </Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,22 @@ import type { NextPage } from "next";
 import { useTranslation, Trans } from "next-i18next";
 import { NextSeo } from "next-seo";
 import Link from "next/link";
-
+import Image from "next/image";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import TransLink from "../components/translation/TransLink";
+
+import AcewillLogo from "/public/assets/portfolio/acewill.png";
+import AcewillImage from "/public/assets/portfolio/portfolioAcewill.webp";
+import GarnetLogo from "/public/assets/portfolio/garnet.svg";
+import GarnetImage from "/public/assets/portfolio/portfolioGarnet.webp";
+import EryesLogo from "/public/assets/portfolio/eryes.png";
+import EryesImage from "/public/assets/portfolio/portfolioEryes.webp";
+import VrccLogo from "/public/assets/portfolio/vrcc.png";
+import VrccImage from "/public/assets/portfolio/portfolioVrcc.webp";
+import NyliumLogo from "/public/assets/portfolio/nylium.png";
+import NyliumImage from "/public/assets/portfolio/portfolioNylium.webp";
+import SwiftpassLogo from "/public/assets/portfolio/vercel.png";
+import SwiftpassImage from "/public/assets/portfolio/portfolioSwiftpass.png";
 
 export async function getStaticProps({ locale }: { locale: string }) {
   return {
@@ -16,6 +29,54 @@ export async function getStaticProps({ locale }: { locale: string }) {
 
 const Home: NextPage = (props) => {
   const { t } = useTranslation("index");
+
+  const projects = [
+    {
+      img: SwiftpassImage,
+      logo: SwiftpassLogo,
+      name: "Swiftpass",
+      description: "Smart password generator which can generate not only strong, but also quick-to-enter passwords.",
+      links: [
+        { text: "Learn more", href: "https://swiftpass.hynekfisera.com/", type: "primary" },
+        { text: "Source code", href: "http://github.com/hynekfisera/swiftpass", type: "secondary" },
+      ],
+    },
+    {
+      img: AcewillImage,
+      logo: AcewillLogo,
+      name: "Acewill",
+      description: "I designed a prototype of the Acewill operating system distributed by Aprocle.",
+      links: [{ text: "Learn more", href: "http://aprocle.com/", type: "secondary" }],
+    },
+    {
+      img: GarnetImage,
+      logo: GarnetLogo,
+      name: "Garnet",
+      description: "I designed a logo for the Garnet operating system",
+      links: [{ text: "Learn more", href: "http://github.com/GarnetOS", type: "secondary" }],
+    },
+    {
+      img: EryesImage,
+      logo: EryesLogo,
+      name: "Eryes",
+      description: "I created livestream overlays and social media posts for the Eryes league",
+      links: [{ text: "Learn more", href: "http://www.twitch.tv/eryesloleague", type: "secondary" }],
+    },
+    {
+      img: VrccImage,
+      logo: VrccLogo,
+      name: "VRCC",
+      description: "I developed a website for the VR Component Constructor project",
+      links: [],
+    },
+    {
+      img: NyliumImage,
+      logo: NyliumLogo,
+      name: "Nylium",
+      description: "I coded a website for the Nylium Survival minecraft server",
+      links: [],
+    },
+  ];
 
   return (
     <>
@@ -42,19 +103,19 @@ const Home: NextPage = (props) => {
         ]}
       />
       <main>
-        <section id="aboutme" className="py-16">
+        <section id="about-me" className="py-12">
           <div className="max-w-screen-lg mx-auto px-4">
             <div className="my-auto">
               <h1 className="text-2xl md:text-3xl font-semibold text-gray-700">
                 <Trans t={t} i18nKey="intro_hello" components={[<span className="text-indigo-500" key={0} />]} />
               </h1>
-              <p className="sm:text-lg text-gray-900 mt-4">
+              <p className="sm:text-lg xl:text-xl text-gray-900 mt-4 xl:mt-5">
                 <Trans t={t} i18nKey="intro_tech" components={[<span className="font-semibold" key={0}></span>, <span className="font-semibold" key={1}></span>, <span className="font-semibold" key={2}></span>]} />
               </p>
-              <p className="sm:text-lg text-gray-900 mt-2">
+              <p className="sm:text-lg xl:text-xl text-gray-900 mt-2 xl:mt-3">
                 <Trans t={t} i18nKey="intro_portfolio" components={[<TransLink href="https://github.com/hynekfisera" className="text-indigo-500 hover:underline" key={0} />]} />
               </p>
-              <div className="flex flex-wrap mt-4 gap-3">
+              <div className="flex flex-wrap mt-4 xl:mt-5 gap-3">
                 <Link href="mailto:hynek@flairleap.com">
                   <a className="btn btn-primary">{t("intro_cta1")}</a>
                 </Link>
@@ -64,6 +125,34 @@ const Home: NextPage = (props) => {
               </div>
             </div>
           </div>
+        </section>
+        <section id="portfolio" className="py-8 max-w-screen-xl mx-auto px-4 xl:px-0">
+          <h2 className="text-center text-3xl sm:text-4xl xl:text-5xl mb-4 lg:mb-0 text-gray-700">Portfolio</h2>
+          {projects.map((project) => (
+            <div key={project.name} className="flex flex-col sm:even:flex-row-reverse sm:flex-row mb-16 sm:mb-12 md:mb-0">
+              <div className="sm:w-1/2 sm:px-4 md:px-16">
+                <Image src={project.img} alt={project.name} />
+              </div>
+              <div className="sm:w-1/2 flex flex-col justify-center">
+                <div className="flex items-center mb-4">
+                  <div className="h-8 w-8 sm:h-10 sm:w-10 xl:h-12 xl:w-12 mr-2 flex items-center justify-center">
+                    <Image src={project.logo} alt={project.name + "logo"} />
+                  </div>
+                  <span className="text-2xl xl:text-3xl font-bold text-gray-800">{project.name}</span>
+                </div>
+                <p className="text-lg sm:text-xl xl:text-2xl mb-2 sm:mb-3">{project.description}</p>
+                <div className="flex gap-3">
+                  {project.links.map((link, i) => {
+                    return (
+                      <Link href={link.href} key={i}>
+                        <a className={`xl:text-xl font-bold transition duration-200 ${link.type === "primary" ? "text-indigo-500 hover:text-indigo-700 underline underline-offset-2" : "text-gray-700 hover:text-gray-800"}`}>{link.text}</a>
+                      </Link>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          ))}
         </section>
       </main>
     </>
