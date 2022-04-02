cf7fcc41d30c050c463b50f10434a9e365c426ee
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2022-04-02 15:27:16 +0200

    Error pages

--- pages/500.tsx
+++ pages/500.tsx
@@ -9,12 +9,12 @@ export async function getStaticProps({ locale }: { locale: string }) {
   };
 }
 
-export default function Custom404() {
+export default function Custom500() {
   const { t } = useTranslation("errorpages");
 
   return (
     <main className="h-[80vh] pt-16">
-      <h1 className="text-center text-2xl">404 - {t("404")}</h1>
+      <h1 className="text-center text-2xl">500 - {t("500")}</h1>
     </main>
   );
 }
