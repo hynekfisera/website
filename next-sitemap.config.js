4b18d42b729a0f9d355280b2db06a223346fcb6b
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2022-04-02 19:53:01 +0200

    Add next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: "https://www.hynekfisera.com",
+  generateRobotsTxt: true,
+  alternateRefs: [
+    {
+      href: "https://www.hynekfisera.com",
+      hreflang: "en",
+    },
+    {
+      href: "https://www.hynekfisera.cz",
+      hreflang: "cs",
+    },
+  ],
+  exclude: ["/404", "/500", "/cs*"],
+};
