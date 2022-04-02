87acfbaee3dc4d50a41a99f877c2192076e722fb
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2022-04-02 20:05:10 +0200

    Add nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,26 @@ import { DefaultSeo } from "next-seo";
 import SEO from "../next-seo.config";
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
+import Router from "next/router";
+import NProgress from "nprogress";
+import { useEffect } from "react";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+
+    Router.events.on("routeChangeStart", handleRouteStart);
+    Router.events.on("routeChangeComplete", handleRouteDone);
+    Router.events.on("routeChangeError", handleRouteDone);
+
+    return () => {
+      Router.events.off("routeChangeStart", handleRouteStart);
+      Router.events.off("routeChangeComplete", handleRouteDone);
+      Router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, []);
+
   return (
     <>
       <DefaultSeo {...SEO} />
