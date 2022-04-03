d4b27cc58a57672641d865d02422d7ebe116ed1c
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2022-04-03 17:34:17 +0200

    Locale prefix redirects

--- next.config.js
+++ next.config.js
@@ -4,6 +4,34 @@ const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   i18n,
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/cs",
+        destination: "https://www.hynekfisera.cz",
+        permanent: true,
+        locale: false,
+      },
+      {
+        source: "/cs/:slug*",
+        destination: "https://www.hynekfisera.cz/:slug*",
+        permanent: true,
+        locale: false,
+      },
+      {
+        source: "/en",
+        destination: "https://www.hynekfisera.com",
+        permanent: true,
+        locale: false,
+      },
+      {
+        source: "/en/:slug*",
+        destination: "https://www.hynekfisera.com/:slug*",
+        permanent: true,
+        locale: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
