43e372adf8705d8a95828abf016990096a20de1d
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2022-04-03 09:46:46 +0200

    Highlight portfolio link

--- pages/links.tsx
+++ pages/links.tsx
@@ -23,46 +23,55 @@ const Links: NextPage = (props) => {
       title: "Portfolio",
       icon: faGlobe,
       href: "/",
+      type: "primary",
     },
     {
       title: "GitHub",
       icon: faGithub,
       href: "https://github.com/hynekfisera",
+      type: "secondary",
     },
     {
       title: "LinkedIn",
       icon: faLinkedin,
       href: "https://linkedin.com/in/hynekfisera",
+      type: "secondary",
     },
     {
       title: "YouTube",
       icon: faYoutube,
       href: "https://youtube.com/phpmyarfi",
+      type: "secondary",
     },
     {
       title: "Twitch",
       icon: faTwitch,
       href: "https://twitch.tv/arfilive",
+      type: "secondary",
     },
     {
       title: "Twitter",
       icon: faTwitter,
       href: "https://twitter.com/hynekfisera",
+      type: "tertiary",
     },
     {
       title: "Instagram",
       icon: faInstagram,
       href: "https://instagram.com/hynekfisera",
+      type: "tertiary",
     },
     {
       title: "Discord",
       icon: faDiscord,
       href: "https://arfi.cz/discord",
+      type: "tertiary",
     },
     {
       title: "Spotify",
       icon: faSpotify,
       href: "https://open.spotify.com/user/hynekfisera",
+      type: "tertiary",
     },
   ];
 
@@ -84,10 +93,14 @@ const Links: NextPage = (props) => {
       <main>
         <section id="links" className="py-8 sm:py-12">
           <h1 className="text-center text-3xl font-semibold mb-8 hidden sm:block">{t("heading")}</h1>
-          <div className="max-w-sm mx-auto px-4">
+          <div className="max-w-sm mx-auto px-4 sm:px-0">
             {links.map((link, i) => (
               <Link href={link.href} key={i}>
-                <a className="mb-4 flex justify-center items-center gap-1 w-full text-lg font-medium text-center text-gray-700 border border-gray-700 rounded-md py-2 transition duration-200 hover:text-white hover:bg-gray-700">
+                <a
+                  className={`mb-4 flex justify-center items-center gap-1 w-full text-lg font-medium text-center border rounded-md py-2 transition duration-200 hover:text-white ${
+                    link.type === "primary" ? "border-indigo-600 text-indigo-600 hover:bg-indigo-500" : "border-gray-600 text-gray-600 hover:bg-gray-600"
+                  }`}
+                >
                   {link.icon && <FontAwesomeIcon icon={link.icon} className="h-4" />} {link.title}
                 </a>
               </Link>
